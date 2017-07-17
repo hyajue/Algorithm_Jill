e3aadd728f62b9f529954e4366361ec3fb6e75d2
Jill Li <hyajue@users.noreply.github.com>
2017-07-16 21:32:57 -0700

    7/16/17

---
+++ LinkedList/ReverseLinkedList.java
@@ -0,0 +1,53 @@
+package LinkedList;
+/**
+* Reverse a singly linked list.
+* 
+* Hint:
+* A linked list can be reversed either iteratively or recursively. Could you implement both?
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+//solution 1 : recursive
+//复杂度
+//时间 O(n) 空间O(n) 
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        return reverse(head, null);
+    }
+    private ListNode reverse(ListNode cur, ListNode prev){
+        if(cur == null) return prev;
+        ListNode next = cur.next;
+        cur.next = prev;
+        return reverse(next, cur);
+    }
+}
+/*
+solution 2 
+复杂度
+时间 O(n) 空间O(1) 
+思路：双指针迭代
+Assume that we have linked list 1 → 2 → 3 → Ø, 
+we would like to change it to Ø ← 1 ← 2 ← 3
+*/ 
+public class ReverseLinkedList {
+	public ListNode reverseList(ListNode head) {
+        if(head == null) return null;
+        ListNode recent = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode next= cur.next;
+            cur.next = recent;
+            recent = cur;
+            cur = next;
+        }
+        return recent;
+    }
+}
