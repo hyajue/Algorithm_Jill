60f756d25c9861c5fc9692684b16c2d40dae0c05
Jill Li <hyajue@users.noreply.github.com>
2017-07-18 13:08:39 -0700

    7/18/17

---
+++ DP/MaximumSubarray.java
@@ -0,0 +1,27 @@
+package DP;
+/**
+* Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+* 
+* For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+* the contiguous subarray [4,-1,2,1] has the largest sum = 6
+*/
+
+/*
+solution 1:
+复杂度：
+时间O(n) 空间O(n)
+思路:dp
+dp[i-1]是否大于0， 是，则加上。否，则dp[i] = nums[i]
+*/
+public class MaximumSubarray {
+	public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int res = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            dp[i] = dp[i-1] > 0 ? dp[i-1] + nums[i] : nums[i];
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
