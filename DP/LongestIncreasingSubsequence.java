59137917a6e7ce57f1f934e5b962ac257ca483f6
Jill Li <hyajue@users.noreply.github.com>
2017-08-03 14:24:30 -0700

    8/3/17

--- DP/LongestIncreasingSubsequence.java
+++ DP/LongestIncreasingSubsequence.java
@@ -11,10 +11,39 @@ Your algorithm should run in O(n2) complexity.
 Follow up: Could you improve it to O(n log n) time complexity?
  */
 
-/*
-Time: O(nlogn)
+/* @Method 1:
+Time: O(nlogn) Space: O(n)
 思路：binary search+dp
+设定一个dp[]数组，来存放结果。eg: dp[i]表示，当有序数组长度为i+1时候，第i+1个数最小是什么。
+顺序traverse该array，用二分法来搜索，当前数字，在dp数组里的位置。如果i跟dp数组的有效size一样长了，那么size++。
+这表示，该数字num比dp[i]还要小，但是整体已经是i+1个数了。这时候size++，表示有效长度更新成了i+2
 */
+/*
+binarySearch(object[ ], int fromIndex, int endIndex, object key);
+如果要搜索的元素key在指定的范围内，则返回搜索键的索引；否则返回-1或者"-"(插入点)。
+
+eg：
+1.该搜索键在范围内，但不在数组中，由1开始计数；
+2.该搜索键在范围内，且在数组中，由0开始计数；
+3.该搜索键不在范围内，且小于范围内元素，由1开始计数；
+4.该搜索键不在范围内，且大于范围内元素，由1开始计数；
+*/
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums == null || nums.length <= 1) 
+            return nums.length;
+        int size = 0;
+        int[] dp = new int[nums.length];
+        for(int num : nums){
+            int i = Arrays.binarySearch(dp, 0, size, num);
+            if(i < 0) i = - (i + 1);
+            dp[i] = num;
+            if(i == size) size++;
+        }
+        return size;
+    }
+}
+
 /*
  Time: O(n^2) Space: O(n)
  思路：dp
