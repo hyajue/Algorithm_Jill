e90cf92a98ce14ad15e024e8ab7caf441bc2949a
Jill Li <hyajue@users.noreply.github.com>
2017-08-10 13:19:12 -0700

    8/10/17

--- Array/SearchInRotatedSortedArrayII.java
+++ Array/SearchInRotatedSortedArrayII.java
@@ -37,7 +37,8 @@ public class SearchInRotatedSortedArrayII {
                     left = mid + 1;
                 else
                     right = mid - 1;
-            }else if(nums[mid] > nums[right]){
+            //因为可能出现重复，所以只有这样才能判断leftside ordered
+	    }else if(nums[mid] > nums[right]){
                 //left side ordered
                 if(target < nums[mid] && target >= nums[left])
                     right = mid - 1;
@@ -50,3 +51,26 @@ public class SearchInRotatedSortedArrayII {
         return false;
     }
 }
+
+//一种好一些的写法如下：
+
+		//If we know for sure right side is sorted or left side is unsorted
+            if (nums[mid] < nums[end] || nums[mid] < nums[start]) {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            //If we know for sure left side is sorted or right side is unsorted
+            } else if (nums[mid] > nums[start] || nums[mid] > nums[end]) {
+                if (target < nums[mid] && target >= nums[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            //If we get here, that means nums[start] == nums[mid] == nums[end], then shifting out
+            //any of the two sides won't change the result but can help remove duplicate from
+            //consideration, here we just use end-- but left++ works too
+            } else {
+                end--;
+            }
