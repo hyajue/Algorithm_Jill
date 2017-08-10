66f8827584a49427ac34af975a039dfe2c1edb44
Jill Li <hyajue@users.noreply.github.com>
2017-08-10 11:38:05 -0700

    8/10/17

--- DFS/BinaryTreeRightSIdeView.java
+++ DFS/BinaryTreeRightSIdeView.java
@@ -42,3 +42,25 @@ public class BinaryTreeRightSIdeView {
         dfs(res, root.left, level + 1);
     }
 }
+
+//BFS: (iterative)
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(root == null) return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            res.add(q.peek().val);
+            for(int i = 0; i < size; i++){
+                TreeNode temp = q.poll();
+                if(temp.right != null)
+                    q.offer(temp.right);
+                if(temp.left != null)
+                    q.offer(temp.left);
+            }
+        }
+        return res;
+    }	
+}
