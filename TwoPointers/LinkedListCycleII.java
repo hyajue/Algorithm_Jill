be568aa48c37546b923b7b0eb659495209979869
Jill Li <hyajue@users.noreply.github.com>
2017-08-11 23:02:27 -0700

    8/11/17

--- TwoPointers/LinkedListCycleII.java
+++ TwoPointers/LinkedListCycleII.java
@@ -14,6 +14,7 @@ package TwoPointers;
 Linked List Cycle的扩展，就是在确定是否有cycle之后还要返回cycle的起始点的位置
 设整个链表长L，环入口与相遇点距离为x，起点到环入口点的距离为a, 环长为r, fast指针在环内已循环了n圈
 a + x = nr
+r = L - a
 a + x = (n – 1)r + r = (n-1)r + L - a
 a = (n-1)r + (L – a – x)
 (L – a – x)为相遇点到环入口点的距离，由此可知，从链表头到环入口点等于(n-1)循环内环+相遇点到环入口点，于是我们从链表头、与相遇点分别设一个指针，
@@ -23,18 +24,18 @@ public class LinkedListCycleII {
 	public ListNode detectCycle(ListNode head) {
 	    if (head == null || head.next == null) return null;
 	    ListNode slow = head;
-		  ListNode fast = head;
-		  while(fast != null && fast.next != null) {
-		    fast = fast.next.next;
+		ListNode fast = head;
+	    while(fast != null && fast.next != null) {
+			fast = fast.next.next;
 		    slow = slow.next;
 		    if (fast == slow) break;
-		  }
-		  if (fast == null || fast.next == null) return null; // no cycle
-		  slow = head;
-		  while(slow != fast) {
+		}
+		if (fast == null || fast.next == null) return null; // no cycle
+		slow = head;
+		while(slow != fast) {
 		    slow = slow.next;
 		    fast = fast.next;
-		  }
-		  return slow;
-	  }
+		}
+		return slow;
+	}
 }
