18d9e7feb2056d257828bacefd63b68ae5a5b1f7
Jill Li <hyajue@users.noreply.github.com>
2017-08-16 21:53:34 -0700

    8/16/17

--- BFS/NestedWeightSumII.java
+++ BFS/NestedWeightSumII.java
@@ -69,4 +69,34 @@ public class NestedWeightSumII {
 }
 
 
-
+//@Method 2: DFS + 2pass
+/*
+第一遍算level，第二遍计算
+*/
+public class Solution {
+    
+    public int depthSumInverse(List<NestedInteger> nestedList) {
+        if(nestedList == null || nestedList.size() == 0) return 0;
+        int h = helper(nestedList);
+        int res = getSum(nestedList, h);
+        return res;
+    }
+    private int getSum(List<NestedInteger> l, int layer) {
+        int sum = 0;
+        if(l == null || l.size() == 0) return sum;
+        for(NestedInteger n : l) {
+            if(n.isInteger()) sum += n.getInteger() * layer;
+            else sum += getSum(n.getList(), layer - 1);
+        }
+        return sum;
+    }
+    private int helper(List<NestedInteger> l) {
+        if(l == null || l.size() == 0) return 0;
+        int max = 0;
+        for(NestedInteger n : l) {
+            if(n.isInteger()) max = Math.max(max, 1);
+            else max = Math.max(max, helper(n.getList()) + 1);
+        }
+        return max;
+    }
+}
