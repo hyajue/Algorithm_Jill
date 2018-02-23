a86b5ffb284a84476949f3aacbc8c85805fce61b
Jill Li <hyajue@users.noreply.github.com>
2018-02-22 17:00:58 -0800

    2/22/2018

--- TwoPointers/IntersectionOfTwoArraysII.java
+++ TwoPointers/IntersectionOfTwoArraysII.java
@@ -17,9 +17,21 @@ What if elements of nums2 are stored on disk, and the memory is limited such tha
 Follow Up:
 
 Q3: 
+
+1. Store the two strings in distributed system(whether self designed or not), then using MapReduce technique to solve the problem;
+
+2. Processing the Strings by chunk, which fits the memory, then deal with each chunk of data at a time;
+
+3. Processing the Strings by streaming, then check.
+
+------------------------------------------------
 If only nums2 cannot fit in memory, put all elements of nums1 into a HashMap, read chunks of array that fit into the memory, and record the intersections.
 
 If both nums1 and nums2 are so huge that neither fit into the memory, sort them individually (external sort), then read 2 elements from each array at a time in memory, record intersections.
+
+I think the second part of the solution is impractical, if you read 2 elements at a time, this procedure will take forever. In principle, we want minimize the number of disk access during the run-time.
+An improvement can be sort them using external sort, read (let’s say) 2G of each into memory and then using the 2 pointer technique, then read 2G more from the array that has been exhausted. Repeat this until no more data to read from disk.
+But I am not sure this solution is good enough for an interview setting. Maybe the interviewer is expecting some solution using Map-Reduce paradigm.
 
 */
 
